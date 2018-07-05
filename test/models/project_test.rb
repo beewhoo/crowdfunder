4c1ae79af448487c039f0d4aec40c856b1171d28
hibernatingnerd <jeffery.mark.campbell@gmail.com>
2018-07-04 23:34:11 -0400

    cleaned up project_test.rb further

--- test/models/project_test.rb
+++ test/models/project_test.rb
@@ -14,25 +14,22 @@ class ProjectTest < ActiveSupport::TestCase
   end
 
   def test_project_start_date_in_the_future_date_in_the_past
-    assert_equal false, build(:project, start_date: Date.yesterday).in_the_future
+    refute false, build(:project, start_date: Date.yesterday).in_the_future
   end
 
   def test_project_start_date_in_the_future
-    assert_equal true, build(:project, start_date: Date.tomorrow).in_the_future
+    assert true, build(:project, start_date: Date.tomorrow).in_the_future
   end
 
   def test_project_end_date_after_start_date
-    assert_equal false, build(:project, start_date: Date.yesterday, end_date: 12.days.ago).end_date_after_start_date
+    refute false, build(:project, start_date: Date.yesterday, end_date: 12.days.ago).end_date_after_start_date
   end
 
   def test_project_goal_number_is_positive
-    assert_equal true, build(:project, goal: 1).goal_positive_number
-
+    assert true, build(:project, goal: 1).goal_positive_number
   end
 
   def test_project_goal_number_is_zero
-
-    assert_equal false, build(:project, goal: 0).goal_positive_number
-
+    refute false, build(:project, goal: 0).goal_positive_number
   end
 end
